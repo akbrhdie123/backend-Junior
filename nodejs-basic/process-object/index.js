74abc498614e61e961d1120020be02284b18b41c
Akbar <akbarhadisubowo9@gmail.com>
2023-11-12 14:32:54 +0700

    test

--- nodejs-basic/process-object/index.js
+++ nodejs-basic/process-object/index.js
@@ -4,7 +4,7 @@ const yourName = process.argv[2] || "Your Name";
 
 const environment = process.env.NODE_ENV || "development";
 
-for(let i = 0; i <= 10000; i++) {
+for (let i = 0; i <= 10000; i++) {
   // Proses looping ini akan membuat penggunaan memori naik
 }
 
@@ -12,4 +12,6 @@ const currentMemoryUsage = process.memoryUsage().heapUsed;
 
 console.log(`Hai, ${yourName}`);
 console.log(`Mode lingkungan: ${environment}`);
-console.log(`Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
+console.log(
+  `Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`
+);
